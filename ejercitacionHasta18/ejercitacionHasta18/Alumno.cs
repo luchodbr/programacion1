3fb1aba35eed6649c3ece131f22dfa82e215b361
luchodbr <36521786+luchodbr@users.noreply.github.com>
2018-08-28 22:10:35 -0300

    Add files via upload

---
+++ ejercitacionHasta18/ejercitacionHasta18/Alumno.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercitacionHasta18
+{
+    class Alumno
+    {
+        private byte nota1;
+        private byte nota2;
+        private float notaFinal;
+        public string apellido;
+        public int legajo;
+        public string nombre;
+
+        public void CalcularFinal()
+        {
+            if (this.nota1 > 4 && this.nota2 > 4)
+            {
+                Random random = new Random();
+                this.notaFinal = random.Next(10);
+
+            }
+        }
+        public void Estudiar(byte notaUno, byte notaDos)
+        {
+            this.nota1 = notaUno;
+            this.nota2 = notaDos;
+        }
+        public Alumno(string nombre, string apellido, int legajo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.legajo = legajo;
+            this.notaFinal = -1;
+        }
+
+        public string mostrar()
+        {
+            if (notaFinal == -1)
+            {
+                return string.Format("el nombre es {0} el apellido es {1} el legajo es {2} y la nota final es 'Alumno desaprobado' ", nombre, apellido, legajo);
+            }
+            return string.Format("el nombre es {0} el apellido es {1} el legajo es {2} y la nota final es {3}", nombre, apellido, legajo, notaFinal);
+
+
+        }
+    }
+}
---
+++ ejercitacionHasta18/ejercitacionHasta18/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercitacionHasta18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno("Luciano","Gil",40901792);
+            //Alumno alumno2 = new Alumno();
+            //Alumno alumno3 = new Alumno();
+            Console.WriteLine("Ingrese nota 1 Alumno 1");
+            string nota1String = Console.ReadLine();
+            byte.TryParse(nota1String, out byte nota1);
+            Console.WriteLine("Ingrese nota 2 Alumno 1");
+            string nota2String = Console.ReadLine();
+            byte.TryParse(nota1String, out byte nota2);
+            alumno1.Estudiar(nota1, nota2);
+            alumno1.CalcularFinal();
+            Console.Write(alumno1.mostrar());
+            Console.Read();
+
+
+
+        }
+    }
+}
